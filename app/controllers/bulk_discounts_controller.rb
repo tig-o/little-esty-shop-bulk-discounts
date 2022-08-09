7f1d7791df86d846544fe2914624a32178b06130
Thiago Silveira <thiago1992@yahoo.com>
2022-08-09 11:36:29 -0600

    Feature: bulk discount controller with new and create

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -7,4 +7,15 @@ class BulkDiscountsController < ApplicationController
     def show
 
     end
+
+    def new
+        @merchant = Merchant.find(params[:merchant_id])
+    end
+
+    def create
+        BulkDiscount.create!(merchant_id: params[:merchant_id], discount: params[:discount], threshold_amount: params[:threshold_amount])
+        redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+    end
+
+
 end
