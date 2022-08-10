986c80edf63a5f2e583a5f21ced3294e28d8ba1f
Thiago Silveira <thiago1992@yahoo.com>
2022-08-10 02:35:32 -0600

    Refactor/Feature: invoice model #discount_amount and #total_discount_revenue

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,7 @@ class Customer < ApplicationRecord
   validates :last_name, presence: true
 
   has_many :invoices
+  has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
 
   # Top 5 customers by transaction count
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,8 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
+  has_many :bulk_discounts, through: :merchant
 
   def best_date
     self.invoices
