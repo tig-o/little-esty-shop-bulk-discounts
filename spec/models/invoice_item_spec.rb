8aa3350d0811ea04f58ff84ae79dd28f9a085043
Thiago Silveira <thiago1992@yahoo.com>
2022-08-10 02:35:01 -0600

    Refactor/Test: invoice model #discount_amount and #total_discount_revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe InvoiceItem do
   describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_one(:merchant).through(:item) }
+    it { should have_many(:bulk_discounts).through(:merchant) }
   end
+
   describe 'instance methods' do
 
     it 'returns total_revenue' do
